595fd6f830591e1380ed852083d3fed8884e62b7
cristinailli <cristina20.illi@gmail.com>
2018-12-05 14:25:23 +0100

    pubsub sub experiment

--- src/main/java/com/github/sauterl/iop2p/sandbox/Experiments.java
+++ src/main/java/com/github/sauterl/iop2p/sandbox/Experiments.java
@@ -1,5 +1,9 @@
 package com.github.sauterl.iop2p.sandbox;
 
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+import io.ipfs.api.IPFS;
+
 /**
  * TODO: write JavaDoc
  *
@@ -7,8 +11,24 @@ package com.github.sauterl.iop2p.sandbox;
  */
 public class Experiments {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
+    IPFS ipfs = new IPFS(args[0]);
+    try {
+      ipfs.pubsub
+          .sub(args[1])
+          .forEach(
+              e -> {
+                System.out.println(e);
+                String a = (String) e.get("com/github/sauterl/iop2p/data");
+                try {
+                  System.out.println("Data: " + new String(Base64.decode(a)));
+                } catch (Base64DecodingException e1) {
+                  e1.printStackTrace();
+                }
+              });
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
-
 }
