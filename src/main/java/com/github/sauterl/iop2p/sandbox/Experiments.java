380fb3c625d36bbee4a9946523cebc1761f12868
loris.sauter <loris.sauter@unibas.ch>
2018-12-05 11:59:35 +0100

    initial gradle setup

---
+++ src/main/java/com/github/sauterl/iop2p/sandbox/Experiments.java
@@ -0,0 +1,14 @@
+package com.github.sauterl.iop2p.sandbox;
+
+/**
+ * TODO: write JavaDoc
+ *
+ * @author loris.sauter
+ */
+public class Experiments {
+
+  public static void main(String[] args){
+
+  }
+
+}
