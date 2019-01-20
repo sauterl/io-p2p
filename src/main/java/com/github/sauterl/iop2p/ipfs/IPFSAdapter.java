f816cf830fe83e1bdd37ce149d5fe2dfec9f5e10
loris.sauter <loris.sauter@unibas.ch>
2019-01-20 23:22:46 +0100

    important stuff

--- src/main/java/com/github/sauterl/iop2p/ipfs/IPFSAdapter.java
+++ src/main/java/com/github/sauterl/iop2p/ipfs/IPFSAdapter.java
@@ -1,25 +1,32 @@
 package com.github.sauterl.iop2p.ipfs;
 
 import io.ipfs.api.IPFS;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.function.Consumer;
+import java.util.Vector;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeoutException;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecuteResultHandler;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.LogOutputStream;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
+import org.zeroturnaround.exec.listener.ProcessDestroyer;
+import org.zeroturnaround.exec.listener.ShutdownHookProcessDestroyer;
 
 /**
- * Wraps the java ipfs instance with it's deamon.
- * Enables to use multiple multiple ipfs instances, e.g. nodes.
- * Requires that {@code ipfs} is a command known to the operating system.
+ * Wraps the java ipfs instance with it's deamon. Enables to use multiple ipfs instances, e.g.
+ * nodes.
  *
  * @author loris.sauter
  */
 public class IPFSAdapter {
+
 
   /*
   Notes:
@@ -32,100 +39,98 @@ public class IPFSAdapter {
    Later: IPFS init and stuff as well
    */
 
-  public static final String IPFS_DAEMON_COMMAND = "ipfs daemon";
+  public static final String IPFS_DAEMON_COMMAND = "daemon";
+  public static final String IPFS_INIT_COMMAND = "init";
   public static final String ENABLE_PUBSUB_FLAG = "--enable-pubsub-experiment";
-
-  public static final String IPFS_INIT_COMMAND = "ipfs init";
-
-  public static final String IPFS_PATH_PREFIX = "IPFS_PATH=";
-
-  private IPFS ipfs;
-
-  private Process daemonProcess;
-
-  private ProcessBuilder processBuilder;
-
-  /**
-   * Creates the default IPFSAdapter.
-   * Expects that ipfs was previously initialized with the command-line call of {@code ipfs init}.
-   */
-  public IPFSAdapter(){
+  public static final String IPFS_ENVIRONMENT_VARIABLE = "IPFS_PATH";
 
 
-    processBuilder = new ProcessBuilder(IPFS_DAEMON_COMMAND, ENABLE_PUBSUB_FLAG);
-    processBuilder.redirectErrorStream(true);
+  private FutureTask<IPFS> futureTask;
+
+  private String ipfsExecPath;
+  private String ipfsRepo = "./ipfs-repo/"; // Default
+
+
+  public static IPFS create(String ipfsExecPath, String ipfsRepo)
+      throws ExecutionException, InterruptedException {
+    IPFSAdapter adapter = new IPFSAdapter(ipfsExecPath, ipfsRepo);
+    return adapter.get();
   }
 
-  public void start() throws IOException {
-    daemonProcess = processBuilder.start();
-    ProcessHandler ph = new ProcessHandler(daemonProcess, this::handleDaemonOutput);
-  }
 
-  private void handleDaemonOutput(String line){
-    System.out.println("[IPFS DAEMON] "+line);
-    if(line.startsWith("API")){
-      String[] parts = line.split(" ");
-        ipfs = new IPFS(parts[parts.length-1]);
-    }
-  }
-
-  private static class ProcessHandler implements Runnable{
-
-    private BufferedReader br;
-    private Thread host;
-    private Consumer<String> handler;
-
-    public ProcessHandler(Process p, Consumer<String> handler){
-      br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      host = new Thread(this);
-      this.handler = handler;
-      host.start();
-    }
-
-    public void stop(){
-      host.interrupt();
-    }
-
-    @Override
-    public void run() {
-      try {
-        String line = "";
-        while ((line = br.readLine()) != null) {
-          handler.accept(line);
-        }
-      }catch(IOException ex){
-        ex.printStackTrace();
-      } finally {
-        try {
-          br.close();
-        } catch (IOException e) {
-          System.err.println("Could not close br");
-          e.printStackTrace();
-        }
+  private IPFSAdapter(String ipfsExecPath, String ipfsRepo) {
+    this.ipfsExecPath = ipfsExecPath;
+    this.ipfsRepo = ipfsRepo;
+    IPFSDaemon daemon = new IPFSDaemon(ipfsExecPath, ipfsRepo);
+    Thread daemonThread = new Thread(daemon);
+    daemonThread.start();
+    Callable<IPFS> callable = () -> {
+      while(daemon.getApiAddress() == null){
+        Thread.sleep(10);
+        System.out.println("ADdr: "+daemon.getApiAddress());
       }
+      return new IPFS(daemon.getApiAddress());
+    };
+    futureTask = new FutureTask<>(callable);
+    ExecutorService executorService = Executors.newFixedThreadPool(4);
+    executorService.execute(futureTask);
+  }
+
+  public IPFS get() throws ExecutionException, InterruptedException {
+    return futureTask.get();
+  }
+
+  @Deprecated
+  private static void create(String path)
+      throws InterruptedException, TimeoutException, IOException {
+    final boolean[] success = {false};
+
+    new ProcessExecutor().command(path, "daemon").redirectOutput(
+        new org.zeroturnaround.exec.stream.LogOutputStream() {
+          @Override
+          protected void processLine(String line) {
+            System.out.println("[DEAMON] " + line);
+            if (line.contains("Error: no IPFS repo found")) {
+              success[0] = false;
+            }
+            if (line.contains("API")) {
+              success[0] = true;
+            }
+          }
+        }).environment("IPFS_PATH", "./ipfs-repo/")
+        .addDestroyer(ShutdownHookProcessDestroyer.INSTANCE).execute(); // blocking -> thread
+
+    System.out.println("IPFS ready: " + success[0]);
+    if (!success[0]) {
+      new ProcessExecutor().command(path, "init").redirectOutput(
+          new org.zeroturnaround.exec.stream.LogOutputStream() {
+            @Override
+            protected void processLine(String line) {
+              System.out.println("[DAEMON] " + line);
+            }
+          }).destroyOnExit().execute();
     }
   }
 
+  public static IPFS startDaemonAndCreate() {
 
-  public static IPFS startDaemonAndCreate(){
     CommandLine commandLine = new CommandLine(IPFS_DAEMON_COMMAND);
     commandLine.addArgument(ENABLE_PUBSUB_FLAG);
     DefaultExecutor executor = new DefaultExecutor();
     DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
 
-    PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(new LogOutputStream(){
-
+    PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(new LogOutputStream() {
 
 
       @Override
       protected void processLine(String line, int logLevel) {
-        System.out.println("[IPFS DAEMON] "+line);
+        System.out.println("[IPFS DAEMON] " + line);
       }
     });
 
     executor.setStreamHandler(pumpStreamHandler);
     try {
-      executor.execute(commandLine,resultHandler);
+      executor.execute(commandLine, resultHandler);
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -141,5 +146,126 @@ public class IPFSAdapter {
     return null;
   }
 
+  private void handleDaemonOutput(String line) {
+    System.out.println("[IPFS DAEMON] " + line);
+    if (line.startsWith("API")) {
+      String[] parts = line.split(" ");
+    }
+  }
+
+  private static class OnCallProcessDestroyer implements ProcessDestroyer {
+
+    private final Vector<Process> processes = new Vector<>();
+
+
+    @Override
+    public boolean add(Process process) {
+      return processes.add(process);
+    }
+
+    @Override
+    public boolean remove(Process process) {
+      return processes.remove(process);
+    }
+
+    @Override
+    public int size() {
+      return processes.size();
+    }
+
+    public void destroyAll() {
+      processes.forEach(p -> {
+        if (p.isAlive()) {
+          p.destroy();
+        }
+      });
+    }
+  }
+
+  private static class IPFSDaemon implements Runnable {
+
+    private final OnCallProcessDestroyer destroyer = new OnCallProcessDestroyer();
+    private String path;
+    private String repo;
+    private String api = null;
+
+    public IPFSDaemon(String path, String repo) {
+      this.path = path;
+      this.repo = repo;
+    }
+
+    @Override
+    public void run() {
+      try {
+        boolean success = executeIpfsDaemon();
+        if(!success){
+          createAndExecuteIpfsDaemon();
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (TimeoutException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+    }
+
+    public String getApiAddress() {
+      if(api == null){
+        return null;
+      }else{
+
+        return api.substring(api.indexOf("/"));
+      }
+    }
+
+    public void stop() {
+      destroyer.destroyAll();
+    }
+
+
+    private boolean executeIpfsDaemon()
+        throws InterruptedException, TimeoutException, IOException {
+      final boolean[] success = {false};
+
+      new ProcessExecutor().command(path, IPFS_DAEMON_COMMAND, ENABLE_PUBSUB_FLAG).redirectOutput(
+          new org.zeroturnaround.exec.stream.LogOutputStream() {
+            @Override
+            protected void processLine(String line) {
+              System.out.println("[DEAMON] " + line);
+              if (line.contains("Error: no IPFS repo found")) {
+                success[0] = false;
+              }
+              if (line.contains("API")) {
+                success[0] = true;
+                api = line;
+              }
+            }
+          }).environment(IPFS_ENVIRONMENT_VARIABLE, repo)
+          .addDestroyer(ShutdownHookProcessDestroyer.INSTANCE).addDestroyer(destroyer)
+          .execute(); // blocking -> thread
+      return success[0];
+    }
+
+    private boolean createAndExecuteIpfsDaemon()
+        throws InterruptedException, TimeoutException, IOException {
+      ProcessResult result = new ProcessExecutor().command(path, IPFS_INIT_COMMAND).redirectOutput(
+          new org.zeroturnaround.exec.stream.LogOutputStream() {
+            @Override
+            protected void processLine(String line) {
+              System.out.println("[INIT] "+line);
+            }
+          }).environment(IPFS_ENVIRONMENT_VARIABLE, repo).destroyOnExit().execute();
+      if(result.getExitValue() == 0){
+        return executeIpfsDaemon();
+      }else{
+        throw new RuntimeException("Couldn't init");
+      }
+    }
+  }
+
+
 
 }
+
--- src/main/java/com/github/sauterl/iop2p/sandbox/Experiments.java
+++ src/main/java/com/github/sauterl/iop2p/sandbox/Experiments.java
@@ -1,7 +1,9 @@
 package com.github.sauterl.iop2p.sandbox;
 
 import com.github.sauterl.iop2p.ipfs.IPFSAdapter;
+import io.ipfs.api.IPFS;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * TODO: write JavaDoc
@@ -11,6 +13,17 @@ import java.io.IOException;
 public class Experiments {
 
   public static void main(String[] args) throws IOException {
-    IPFSAdapter.startDaemonAndCreate();
+    try {
+      //IPFSAdapter.create("C:/Users/loris/uni/11_hs18/internet-overloards/go-ipfs/ipfs.exe");
+      IPFS ipfs = IPFSAdapter.create("C:/Users/loris/uni/11_hs18/internet-overloards/go-ipfs/ipfs.exe", "./ipfs-repo");
+      ipfs.pubsub.pub("asdf", "asdf");
+      System.out.println("ffff");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
